09f82119ba32ed73aae670cbafa983bd800c5279
marcingal <marcin921125@gmail.com>
2018-11-27 10:57:28 +0100

    clearing text field after adding a task

--- src/App.js
+++ src/App.js
@@ -4,34 +4,43 @@ import { TextField, RaisedButton } from 'material-ui'
 class App extends Component {
   state = {
     tasks: [
-      { taskName: 'Odkurzanie', completed: false},
-      { taskName: 'Zmywanie naczyń', completed: false}
+      { taskName: 'Odkurzanie', completed: false },
+      { taskName: 'Zmywanie naczyń', completed: false }
     ],
     taskName: ''
   }
 
-handleChange = (event) => {
-  this.setState({ taskName: event.target.value})
-}
+  handleChange = (event) => {
+    this.setState({ taskName: event.target.value })
+  }
 
-handleClick = (event) => {
-  let tasks =  this.state.tasks
-  tasks.push({taskName: this.state.taskName, completed: false})
-  this.setState( {tasks })
-}
+  handleClick = (event) => {
+    let tasks = this.state.tasks
+    tasks.push({ taskName: this.state.taskName, completed: false })
+    this.setState({ tasks, taskName: '' })
+
+  }
 
   render() {
     return (
       <div className="App">
         <div>
-          <TextField hintText="Put your text" onChange={this.handleChange}></TextField>
-          <RaisedButton label="Add" primary={true} onClick={this.handleClick}></RaisedButton>
+          <TextField
+            hintText="Put your text"
+            value={this.state.taskName}
+            onChange={this.handleChange}>
+          </TextField>
+          <RaisedButton
+            label="Add"
+            primary={true}
+            onClick={this.handleClick}>
+          </RaisedButton>
         </div>
         <div>
           {this.state.tasks.map((task, index) => (
-          <div>{task.taskName}</div>
+            <div>{task.taskName}</div>
           ))}
-      </div>
+        </div>
       </div>
     )
   }
